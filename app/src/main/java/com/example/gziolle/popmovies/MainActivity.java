489aa391015cab22ecabc76e70300f57d0bdffc2
Guilherme Ziolle <gziolle@gmail.com>
2016-10-18 13:18:40 -0200

    1.01 - Initial commit

---
+++ app/src/main/java/com/example/gziolle/popmovies/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.gziolle.popmovies;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new MovieListFragment()).commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.settings:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
